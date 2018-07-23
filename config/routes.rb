4ba54e5961cbe0cdc0dd2ef29459a26d500f22e8
kouuuki <mark.web.888@gmail.com>
2018-07-23 22:01:12 +0900

    add markdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'comments#index'
-
+  namespace :api, format: 'json' do
+    namespace :v1  do
+      resources :comments
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
